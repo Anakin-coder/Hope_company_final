753fe5761298bc19669fd507a79e03c27bfb5b3a
Enio Gruppi Neto <enio.neto@aluno.faculdadeimpacta.com.br>
2020-05-06 22:04:12 -0300

    Alterações nas consultas da tabela pedido

--- Hope-Company/clientes.py
+++ Hope-Company/clientes.py
@@ -28,11 +28,7 @@ def listar_clientes_api():
 
 @app.route("/cliente/novo/", methods = ["GET"])
 def form_criar_cliente_api():
-<<<<<<< HEAD
     return render_template("form_cliente.html", id_cliente = "novo", nome = "", sexo = "", telefone = "", endereco = "", email ="",)
-=======
-    return render_template("form_cliente.html", id_cliente = "novo", nome = "", sexo = "", telefone = "", endereco = "", email = "")
->>>>>>> e194429e570e9255bcb03511a0e708b5677bb314
 
 @app.route("/cliente/novo/", methods = ["POST"])
 def criar_cliente_api():    
@@ -40,24 +36,13 @@ def criar_cliente_api():
     sexo = request.form["sexo"]
     telefone = request.form["telefone"]
     endereco = request.form["endereco"]
-<<<<<<< HEAD
     endereco = request.form["email"]
     id_cliente = criar_cliente(nome, sexo, telefone, endereco, email)
     return render_template("menu.html", mensagem = f"{'O' if sexo == 'M' else 'A'} cliente {nome} foi criad{'o' if sexo == 'M' else 'a'} com o id {id_cliente}.")
-=======
-    email = request.form["email"]
-    id_cliente = criar_cliente(nome, sexo, telefone, endereco, email)
-    cliente = consultar_cliente(id_cliente)
-    if cliente == None:
-        return render_template("menu.html", mensagem = f"Cliente já cadastrado!"), 404
-    return render_template("menu.html", mensagem = f"Novo cliente criado: {id_cliente}.")
->>>>>>> e194429e570e9255bcb03511a0e708b5677bb314
 
 @app.route("/cliente/<int:id_cliente>/", methods = ["GET"])
 def form_alterar_cliente_api(id_cliente):
     cliente = consultar_cliente(id_cliente)
-    if cliente == None:
-        return render_template("menu.html", mensagem = f"Esse cliente não existe."), 404
     return render_template("form_cliente.html", id_cliente = id_cliente, nome = cliente['nome'], sexo = cliente['sexo'], telefone = cliente['telefone'], endereco = cliente['endereco'], email = cliente['email'])
 
 @app.route("/cliente/<int:id_cliente>/", methods = ["POST"])
@@ -195,28 +180,7 @@ def rows_to_dict(description, rows):
 ####################################
 #### Definições básicas de DAO. ####
 ####################################
-<<<<<<< HEAD
 #  IF NOT EXISTS
-'''
-
-CREATE TABLE IF NOT EXISTS pedido (
-    id_pedido INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_cliente INTEGER NOT NULL,
-    datahora DATETIME NOT NULL,
-    pago TINYINT(1),
-    FOREIGN KEY(id_cliente) REFERENCES cliente(id_cliente)
-);
-CREATE TABLE  IF NOT EXISTS produto_pedido (
-    id_produto INTEGER PRIMARY KEY,
-    id_pedido INTEGER PRIMARY KEY,
-    preco_unitario REAL NOT NULL,
-    quantidade INTEGER NOT NULL,
-    FOREIGN KEY(id_produto) REFERENCES produto(id_produto),
-    FOREIGN KEY(id_pedido) REFERENCES pedido(id_pedido)
-);
-
-'''
-
 sql_create = """
 CREATE TABLE  IF NOT EXISTS cliente (
     id_cliente INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -225,44 +189,24 @@ CREATE TABLE  IF NOT EXISTS cliente (
     telefone CHAR(11) NULL,
     endereco CHAR(100) NULL,
     email CHAR(100) NULL
-=======
-#  IF NOT EXISTS  #
-
-
-sql_create = """
-CREATE TABLE IF NOT EXISTS cliente (
-    id_cliente INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome VARCHAR(50),
-    sexo VARCHAR(1),
-    telefone CHAR(11),
-    endereco CHAR(100),
-    email CHAR(100) 
->>>>>>> e194429e570e9255bcb03511a0e708b5677bb314
 );
 
 CREATE TABLE IF NOT EXISTS produto (
     id_produto INTEGER PRIMARY KEY AUTOINCREMENT,
     descricao VARCHAR(50) NOT NULL,
-<<<<<<< HEAD
+    quantidade INTEGER,
+    cores VARCHAR(50),
     preco REAl NOT NULL
-=======
-    quantidade INT,
-    preco REAL,
-    cores VARCHAR(50) NOT NULL
-
 );
+
 CREATE TABLE IF NOT EXISTS pedido (
     id_pedido INTEGER PRIMARY KEY AUTOINCREMENT,
     id_cliente INTEGER NOT NULL,
-    cpf_cliente INTERGER NOT NULL,
-    descricao_produto VARCHAR NOT NULL,
-    quantidade  INTERGER NOT NULL,
-    cores VARCHAR(50) NOT NULL
     datahora DATETIME NOT NULL,
-    status TINYINT(1),
+    pago TINYINT(1),
     FOREIGN KEY(id_cliente) REFERENCES cliente(id_cliente)
->>>>>>> e194429e570e9255bcb03511a0e708b5677bb314
 );
+
 CREATE TABLE IF NOT EXISTS produto_pedido (
     id_produto INTEGER PRIMARY KEY,
     id_pedido INTEGER,
@@ -271,7 +215,6 @@ CREATE TABLE IF NOT EXISTS produto_pedido (
     FOREIGN KEY(id_produto) REFERENCES produto(id_produto),
     FOREIGN KEY(id_pedido) REFERENCES pedido(id_pedido)
 );
-
 """
 
 def conectar():
@@ -284,7 +227,6 @@ def criar_bd():
         con.commit()
 
 #_______CLIENTES________#
-
 def criar_cliente(nome, sexo, telefone, endereco, email):
     with closing(conectar()) as con, closing(con.cursor()) as cur:
         cur.execute("INSERT INTO cliente (nome, sexo, telefone, endereco, email) VALUES (?, ?, ?, ?, ?)", (nome, sexo, telefone, endereco, email))
@@ -302,140 +244,75 @@ def listar_clientes():
         cur.execute("SELECT id_cliente, nome, sexo, telefone, endereco, email FROM cliente ORDER BY id_cliente")
         return rows_to_dict(cur.description, cur.fetchall())
 
-#def listar_clientes_ordem():
-#    with closing(conectar()) as con, closing(con.cursor()) as cur:
-#        cur.execute("SELECT id_cliente, nome, sexo, telefone, endereco, email FROM cliente ORDER BY nome")
-#        return rows_to_dict(cur.description, cur.fetchall())
 
 def editar_cliente(id_cliente, nome, sexo, telefone, endereco, email):
     with closing(conectar()) as con, closing(con.cursor()) as cur:
-<<<<<<< HEAD
-        cur.execute("SELECT id_cliente, nome, sexo, telefone, endereco, email FROM cliente ORDER BY nome")
-        return rows_to_dict(cur.description, cur.fetchall())
-=======
         cur.execute("UPDATE cliente SET nome = ?, sexo = ?, telefone = ?, endereco = ?, email = ? WHERE id_cliente = ?", (nome, sexo, telefone, endereco, email, id_cliente))
         con.commit()
->>>>>>> e194429e570e9255bcb03511a0e708b5677bb314
 
 def deletar_cliente(id_cliente):
     with closing(conectar()) as con, closing(con.cursor()) as cur:
         cur.execute("DELETE FROM cliente WHERE id_cliente = ?", (id_cliente, ))
         con.commit()
-
-
     
 #_________PRODUTOS__________#
 
-def criar_produto(descricao, quantidade, preco, cores):
+def criar_produto(descricao, quantidade, cores, preco):
     with closing(conectar()) as con, closing(con.cursor()) as cur:
-<<<<<<< HEAD
-        cur.execute("SELECT id_cliente, nome, sexo, telefone, endereco, email FROM cliente WHERE id_cliente = ?", (id_cliente, ))
-        return row_to_dict(cur.description, cur.fetchone())
-
-def consultar_produto(id_produto):
-    with closing(conectar()) as con, closing(con.cursor()) as cur:
-        cur.execute("SELECT p.id_produto, p.descricao, p.preco FROM produto p INNER JOIN cliente c ON p.id_cliente = c.id_cliente WHERE id_produto = ?", (id_produto, ))
-=======
-        cur.execute("INSERT INTO produto (descricao, quantidade, preco, cores) VALUES (?, ?, ?, ?)", (descricao, quantidade, preco, cores))
+        cur.execute("INSERT INTO produto (descricao, quantidade, cores, preco) values(?, ?, ?, ?)", (descricao, quantidade, cores, preco))
         id_produto = cur.lastrowid
-        con.commit()
-        return id_produto
+        return row_to_dict(cur.description, cur.fetchone())
 
 def consultar_produto(id_produto):
     with closing(conectar()) as con, closing(con.cursor()) as cur:
         cur.execute("SELECT id_produto, descricao, quantidade, preco, cores FROM produto WHERE id_produto = ?", (id_produto, ))
->>>>>>> e194429e570e9255bcb03511a0e708b5677bb314
         return row_to_dict(cur.description, cur.fetchone())
 
 def listar_produtos():
     with closing(conectar()) as con, closing(con.cursor()) as cur:
-<<<<<<< HEAD
-        cur.execute("SELECT p.id_produto, p.descricao, p.preco FROM produto p INNER JOIN cliente c ON p.id_cliente = c.id_cliente ORDER BY p.id_produto")
+        cur.execute("SELECT p.id_produto, p.descricao, p.preco, p.quantidade, p.cores FROM produto p ORDER BY p.id_produto")
         return rows_to_dict(cur.description, cur.fetchall())
         
-
-def criar_cliente(nome, sexo, telefone, endereco, email):
-    with closing(conectar()) as con, closing(con.cursor()) as cur:
-        cur.execute("INSERT INTO cliente (nome, sexo, telefone, endereco, email) VALUES (?, ?, ?, ?, ?)", (nome, sexo, telefone, endereco, email))
-        id_cliente = cur.lastrowid
-=======
-        cur.execute("SELECT id_produto, descricao, quantidade, preco, cores FROM produto ORDER BY id_produto")
-        return rows_to_dict(cur.description, cur.fetchall())    
-
-#def listar_produtos_ordem():
-#    with closing(conectar()) as con, closing(con.cursor()) as cur:
-#        cur.execute("SELECT id_produto, descricao, quantidade, preco, cores FROM produto ORDER BY descrição")
-#        return rows_to_dict(cur.description, cur.fetchall())
-
 def editar_produto(id_produto, descricao, quantidade, preco, cores):
     with closing(conectar()) as con, closing(con.cursor()) as cur:
         cur.execute("UPDATE produto SET descricao = ?, quantidade = ?, preco = ?, cores = ? WHERE id_produto = ?", (descricao, quantidade, preco, cores, id_produto))
->>>>>>> e194429e570e9255bcb03511a0e708b5677bb314
         con.commit()
 
-<<<<<<< HEAD
-def criar_produto(descricao, id_cliente, preco):
-    with closing(conectar()) as con, closing(con.cursor()) as cur:
-        cur.execute("INSERT INTO produto (descricao, id_cliente, preco) VALUES (?, ?, ?)", (descricao, id_cliente, preco))
-        id_produto = cur.lastrowid
-=======
 def deletar_produto(id_produto):
     with closing(conectar()) as con, closing(con.cursor()) as cur:
         cur.execute("DELETE FROM produto WHERE id_produto = ?", (id_produto, ))
->>>>>>> e194429e570e9255bcb03511a0e708b5677bb314
         con.commit()
 
-<<<<<<< HEAD
-def editar_cliente(id_cliente, nome, sexo, telefone, endereco, email):
-    with closing(conectar()) as con, closing(con.cursor()) as cur:
-        cur.execute("UPDATE cliente SET nome = ?, sexo = ?, telefone = ?, endereco = ?, email = ?, WHERE id_cliente = ?", (nome, sexo, telefone, endereco, id_cliente, email))
-=======
 #_________PEDIDOS__________#
 
-def criar_pedido(cpf_cliente, descricao, quantidade, cores, datahora, status):
+def criar_pedido(id_cliente, datahora, status):
     with closing(conectar()) as con, closing(con.cursor()) as cur:
-        cur.execute( "INSERT INTO p.pedido (p.descricao, p.quantidade, p.cores, p.datahora, p.status) VALUES (?, ?, ?, ?, ?,)", (descricao, quantidade, cores, datahora, status))
+        cur.execute("INSERT INTO pedido (p.id_cliente, p.datahora, p.status) VALUES (?, ?, ?) WHERE p.id_cliente = ?", (datahora, status, id_cliente))
         id_pedido = cur.lastrowid
->>>>>>> e194429e570e9255bcb03511a0e708b5677bb314
         con.commit()
         return id_pedido
 
-<<<<<<< HEAD
-def editar_produto(id_produto, descricao, preco):
-    with closing(conectar()) as con, closing(con.cursor()) as cur:
-        cur.execute("UPDATE produto SET descricao = ?, id_cliente = ?, preco = ? WHERE id_produto = ?", (descricao, id_cliente, preco))
-        con.commit()
-=======
 def consultar_pedido(id_pedido):
     with closing(conectar()) as con, closing(con.cursor()) as cur:
-        cur.execute("SELECT p.id_pedido, c.nome,  p.descricao, p.quantidade, p.cores, p.datahora, p.status FROM pedido p INNER JOIN cliente c ON p.id_cliente = c.id_cliente WHERE p.id_pedido  = ?", (id_pedido, ))
+        cur.execute("SELECT p.id_cliente, p.id_pedido, p.datahora, p.status FROM pedido p WHERE p.id_pedido  = ?", (id_pedido, ))
         return row_to_dict(cur.description, cur.fetchone())
->>>>>>> e194429e570e9255bcb03511a0e708b5677bb314
 
 def listar_pedidos():
     with closing(conectar()) as con, closing(con.cursor()) as cur:
-        cur.execute("SELECT p.id_pedido, c.nome,  p.descricao, p.quantidade, p.cores, p.datahora, p.status FROM pedido p INNER JOIN cliente c ON p.id_cliente = c.id_cliente ORDER BY p.id_pedido")
+        cur.execute("SELECT p.id_cliente, p.id_pedido, p.datahora, p.status FROM pedido ORDER BY p.id_pedido")
         return rows_to_dict(cur.description, cur.fetchall())    
-
-#def listar_produtos_ordem():
-#    with closing(conectar()) as con, closing(con.cursor()) as cur:
-#        cur.execute("SELECT id_produto, descricao, quantidade, preco, cores FROM produto ORDER BY descrição")
-#        return rows_to_dict(cur.description, cur.fetchall())
-
-def editar_pedido(id_pedido, descricao, quantidade, cores, status):
-    with closing(conectar()) as con, closing(con.cursor()) as cur:
-        cur.execute("UPDATE pedido SET descricao = ?, quantidade = ?, cores = ?, status = ? WHERE id_pedido = ?", (descricao, quantidade, cores, status, id_pedido))
-        con.commit()
 
 def deletar_pedido(id_pedido):
     with closing(conectar()) as con, closing(con.cursor()) as cur:
         cur.execute("DELETE FROM pedido WHERE id_pedido = ?", (id_pedido, ))
         con.commit()
 
+#_________PRODUTO PEDIDO__________#
+
+
 ########################
 #### Inicialização. ####
 ########################
 
 if __name__ == "__main__":
-    criar_bd
-    app.run()
+    criar_cliente()
